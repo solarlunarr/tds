2842da1d0382ea8545cf559c53335203de99a287
solarlunarr <solarlunarrrr@gmail.com>
2026-08-12 10:55:42 -0400

    Add files via upload
    Balancing changes to operations

--- TDS.py
+++ TDS.py
@@ -31,37 +31,37 @@ targeting_item = None
 targeting_slot_index = None
 
 # Images
-map_image = pg.image.load('assets/Map1.png').convert_alpha()
+map_image = pg.image.load('tds/assets/Map1.png').convert_alpha()
 enemy_images = {
-    "weak": pg.image.load('assets/enemy.png').convert_alpha(),
-    "strong": pg.image.load('assets/enemy2.png').convert_alpha()
+    "weak": pg.image.load('tds/assets/enemy.png').convert_alpha(),
+    "strong": pg.image.load('tds/assets/enemy2.png').convert_alpha()
 }
 
 # Load Boss Asset
 try:
-    boss_image = pg.image.load('assets/boss.png').convert_alpha()
+    boss_image = pg.image.load('tds/assets/boss.png').convert_alpha()
 except:
-    boss_image = pg.image.load('assets/boss.png').convert_alpha()
+    boss_image = pg.image.load('tds/assets/boss.png').convert_alpha()
 boss_image = pg.transform.rotate(boss_image, 90)
 boss_image = pg.transform.scale(boss_image, (90, 90))
 enemy_images["boss"] = boss_image
 
 
-IceTurret = pg.image.load('assets/IceTurret.png').convert_alpha()
-buy_IceTurret_image = pg.image.load('assets/IceTurret.png').convert_alpha()
-cancel_image = pg.image.load('assets/Cancel.png').convert_alpha()
+IceTurret = pg.image.load('tds/assets/IceTurret.png').convert_alpha()
+buy_IceTurret_image = pg.image.load('tds/assets/IceTurret.png').convert_alpha()
+cancel_image = pg.image.load('tds/assets/Cancel.png').convert_alpha()
 scaled_cancel = pg.transform.scale(cancel_image, (30,30))
-upgrade_image = pg.image.load('assets/Upgrade.png').convert_alpha()
+upgrade_image = pg.image.load('tds/assets/Upgrade.png').convert_alpha()
 scaled_upgrade = pg.transform.scale(upgrade_image, (100,30))
-start_image = pg.image.load('assets/Start.png').convert_alpha()
+start_image = pg.image.load('tds/assets/Start.png').convert_alpha()
 scaled_start = pg.transform.scale(start_image, (125,50))
-restart_image = pg.image.load('assets/Restart.png').convert_alpha()
+restart_image = pg.image.load('tds/assets/Restart.png').convert_alpha()
 scaled_restart = pg.transform.scale(restart_image, (75,30))
-bullet_image = pg.image.load('assets/Bullet.png').convert_alpha()
-speed_image = pg.image.load('assets/Speed 1.png').convert_alpha()
+bullet_image = pg.image.load('tds/assets/Bullet.png').convert_alpha()
+speed_image = pg.image.load('tds/assets/Speed.png').convert_alpha()
 scaled_speed = pg.transform.scale(speed_image, (125,50))
 
-with open('assets/Map1.tmj') as file:
+with open('tds/assets/Map1.tmj') as file:
     world_data = json.load(file)
 
 text_font = pg.font.SysFont("Consolas", 20, bold=True)
@@ -136,7 +136,7 @@ start_button = Button(c.WIDTH + 175, 10, scaled_start, True)
 restart_button = Button(550, 500, scaled_restart, True)
 speed_button = Button(c.WIDTH + 175, 40, scaled_speed, False)
 
-logo_image = pg.image.load('assets/logo.webp').convert_alpha()
+logo_image = pg.image.load('tds/assets/logo.webp').convert_alpha()
 scaled_logo = pg.transform.scale(logo_image, (125, 200))
 
 dragged_item = None
@@ -205,7 +205,10 @@ while run:
             level_started = False
             last_enemy_spawn = pg.time.get_ticks()
             world.reset_level()
-            world.process_enemies()
+            if world.level >= 5:
+                game_outcome = 1
+            else:
+                world.process_enemies()
 
         draw_text(str(c.BUY_COST), text_font, "grey100", c.WIDTH + 35, 250)
 
@@ -250,6 +253,7 @@ while run:
             enemy_group.empty()
             turret_group.empty()
             bullet_group.empty()
+            
 
     if dragged_item:
         dragged_item.draw(screen, pos=pg.mouse.get_pos())
--- turret_data.py
+++ turret_data.py
@@ -2,25 +2,25 @@ TURRET_DATA = [
     {
         # Level 1
         "range": 100,
-        "cooldown": 1500,
-        "damage": 10
+        "cooldown": 1250,
+        "damage": 5
     },
     {
         # Level 2
         "range": 125,
-        "cooldown": 1250,
-        "damage": 15
+        "cooldown": 1000,
+        "damage": 7
     },
     {
         # Level 3
         "range": 150,
-        "cooldown": 1000,
-        "damage": 20
+        "cooldown": 800,
+        "damage": 10
     },
     {
         # Level 4
         "range": 180,
-        "cooldown": 800,
-        "damage": 30
+        "cooldown": 500,
+        "damage": 15
     }
 ]
